c4839f1f9cf422206188d90a0774afedd6f9903a
kiddynamo <njenan@gmail.com>
2016-05-10 20:44:31 -0700

    Angular reading from api.

--- src/main/java/com/example/YourFirstAPI.java
+++ src/main/java/com/example/YourFirstAPI.java
@@ -1,8 +1,22 @@
 package com.example;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+
 /**
-  * Add your first API methods in this class, or you may create another class. In that case, please
-  * update your web.xml accordingly.
+ * Add your first API methods in this class, or you may create another class. In that case, please
+ * update your web.xml accordingly.
  **/
+@Api(
+        name = "jobs",
+        version = "v1"
+)
 public class YourFirstAPI {
+
+    @ApiMethod(httpMethod = ApiMethod.HttpMethod.GET, name = "helloWorld", path = "hello")
+    public HelloWorld helloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setText("Hello World");
+        return helloWorld;
+    }
 }
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,3 +1,6 @@
-angular.module('gdg-gae-presentation', []).controller('controller', function ($scope) {
-   $scope.text = "Hello World";
+angular.module('gdg-gae-presentation', []).controller('controller', function ($scope, $http) {
+    $scope.text = "Loading...";
+    $http.get('/_ah/api/jobs/v1/hello').then(function (resp) {
+        $scope.text = resp.data.text;
+    });
 });
